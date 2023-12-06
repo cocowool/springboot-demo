c2a4754b8589a7df372dc859a2260e0def88202d
cocowool <cocowool@gmail.com>
2023-12-06 18:32:31 +0800

    test springboot project

--- springboot-demo-03/src/main/java/cn/edulinks/springbootdemo/App.java
+++ springboot-demo-03/src/main/java/cn/edulinks/springbootdemo/App.java
@@ -1,13 +1,22 @@
 package cn.edulinks.springbootdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+//    @Override
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        LOG.debug("this is debug message ... ");
+
     }
 }
